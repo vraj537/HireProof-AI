70cece10bdd10a8985d661c888e75baa25576ef2
vraj537 <218786909+vraj537@users.noreply.github.com>
2026-09-04 21:22:18 +0530

    commit

--- hireproof/settings.py
+++ hireproof/settings.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 BASE_DIR=Path(__file__).resolve().parent.parent
 SECRET_KEY=os.getenv("DJANGO_SECRET_KEY","dev-only-change-me")
 DEBUG=os.getenv("DEBUG","True").lower()=="true"
